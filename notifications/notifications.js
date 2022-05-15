a04b0e1046f2e0fbc6fec9e173779cbe730377dc
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-05-15 18:35:00 +0300

    upd

---
+++ notifications/notifications.js
@@ -0,0 +1,10 @@
+import notificationsCSS from "/notifications/notifications.css";
+
+export default function notifications() {
+  return (
+    <div
+      className="container-fluid notifications-section"
+      style={notificationsCSS}
+    ></div>
+  );
+}
